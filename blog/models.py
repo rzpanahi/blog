74100c50c3a18543cb78d6874622cd000a8db4e0
Reza Panahi <rezapanahiads2@gmail.com>
2025-08-18 11:12:53 +0330

    add tag functionality

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 
+from taggit.managers import TaggableManager
+
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -27,6 +29,7 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
 
     class Meta:
         ordering = ["-publish"]
